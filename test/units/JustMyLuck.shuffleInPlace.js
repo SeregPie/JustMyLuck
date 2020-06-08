fd53276d46e1efc2be651a1e08691ebffe34fb8b
Sergej Sintschilin <seregpie@gmail.com>
2020-06-08 07:53:03 +0200

    remove duplicate line breaks

--- test/units/JustMyLuck.shuffleInPlace.js
+++ test/units/JustMyLuck.shuffleInPlace.js
@@ -2,7 +2,6 @@ let assert = require('assert').strict;
 
 let JustMyLuck = require('../../index');
 
-
 module.exports = function() {
 	let array = [1, 2, 3, 4, 5];
 	let shuffledArray = JustMyLuck.shuffleInPlace(array);
