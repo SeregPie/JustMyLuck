3305123e4a2ab0a4498f18f2c605c25b5ce56b37
Sergej Sintschilin <seregpie@gmail.com>
2020-06-08 08:39:49 +0200

    typo

--- src/JustMyLuck.booleanWeighted.js
+++ src/JustMyLuck.booleanWeighted.js
@@ -3,7 +3,7 @@ import JustMyLuck from './JustMyLuck';
 JustMyLuck.extend({
 	booleanWeighted(weight) {
 		if (weight > 0) {
-			if (weight < infinity) {
+			if (weight < Infinity) {
 				return (1 - this.random()) * (1 + weight) > 1;
 			}
 			return true;
