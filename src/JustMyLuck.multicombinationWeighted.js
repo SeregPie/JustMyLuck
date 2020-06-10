dfc585a1b8affba2715bf178c15104471104b499
Sergej Sintschilin <seregpie@gmail.com>
2020-06-10 10:32:28 +0200

    handle special weight caes in multicombination and multipermutation

--- src/JustMyLuck.multicombinationWeighted.js
+++ src/JustMyLuck.multicombinationWeighted.js
@@ -1,15 +1,38 @@
+import './JustMyLuck.multicombination';
 import './JustMyLuck.singleWeighted';
 import JustMyLuck from './JustMyLuck';
 
-import Array_fromExcept from './core/Array/fromExcept';
+import Array_fromExceptLike from './core/Array/fromExceptLike';
 import Array_prototype_multicombinations from './core/Array/prototype/multicombinations';
+import Array_prototype_sum from './core/Array/prototype/sum';
 
 JustMyLuck.extend({
-	multicombinationWeighted(weightedCollection, count) {
-		let weightedArray = Array_fromExcept(weightedCollection);
-		return this.singleWeighted(Array_prototype_multicombinations(weightedArray, count).map(v => [
-			v.map(v => v[0]),
-			v.map(v => v[1]).reduce((r, n) => r + n, 0),
-		]));
+	multicombinationWeighted(weightedCollection, k) {
+		let weightedArray = Array_fromExceptLike(weightedCollection);
+		let overweightedValues = [];
+		let normalWeightedArray = [];
+		let underweightedValues = [];
+		for (let i = 0, n = weightedArray.length; i < n; i++) {
+			let [value, weight] = weightedArray[i];
+			if (weight > 0) {
+				if (weight < Infinity) {
+					normalWeightedArray.push([value, weight]);
+				} else {
+					overweightedValues.push(value);
+				}
+			} else {
+				underweightedValues.push(value);
+			}
+		}
+		if (overweightedValues.length) {
+			return this.multicombination(overweightedValues, k);
+		}
+		if (normalWeightedArray.length) {
+			return this.singleWeighted(Array_prototype_multicombinations(normalWeightedArray, k).map(v => [
+				v.map(v => v[0]),
+				Array_prototype_sum(v.map(v => v[1])),
+			]));
+		}
+		return this.multicombination(underweightedValues, k);
 	},
 });
--- src/JustMyLuck.multipermutationWeighted.js
+++ src/JustMyLuck.multipermutationWeighted.js
@@ -1,15 +1,38 @@
+import './JustMyLuck.multipermutation';
 import './JustMyLuck.singleWeighted';
 import JustMyLuck from './JustMyLuck';
 
-import Array_fromExcept from './core/Array/fromExcept';
+import Array_fromExceptLike from './core/Array/fromExceptLike';
 import Array_prototype_multipermutations from './core/Array/prototype/multipermutations';
+import Array_prototype_sum from './core/Array/prototype/sum';
 
 JustMyLuck.extend({
-	multipermutationWeighted(weightedCollection, count) {
-		let weightedArray = Array_fromExcept(weightedCollection);
-		return this.singleWeighted(Array_prototype_multipermutations(weightedArray, count).map(v => [
-			v.map(v => v[0]),
-			v.map(v => v[1]).reduce((r, n) => r + n, 0),
-		]));
+	multipermutationWeighted(weightedCollection, k) {
+		let weightedArray = Array_fromExceptLike(weightedCollection);
+		let overweightedValues = [];
+		let normalWeightedArray = [];
+		let underweightedValues = [];
+		for (let i = 0, n = weightedArray.length; i < n; i++) {
+			let [value, weight] = weightedArray[i];
+			if (weight > 0) {
+				if (weight < Infinity) {
+					normalWeightedArray.push([value, weight]);
+				} else {
+					overweightedValues.push(value);
+				}
+			} else {
+				underweightedValues.push(value);
+			}
+		}
+		if (overweightedValues.length) {
+			return this.multipermutation(overweightedValues, k);
+		}
+		if (normalWeightedArray.length) {
+			return this.singleWeighted(Array_prototype_multipermutations(normalWeightedArray, k).map(v => [
+				v.map(v => v[0]),
+				Array_prototype_sum(v.map(v => v[1])),
+			]));
+		}
+		return this.multipermutation(underweightedValues, k);
 	},
 });
