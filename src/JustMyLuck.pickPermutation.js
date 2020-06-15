490a0774107ac1d5a62031b81ef4d0ef78d8476b
Sergej Sintschilin <seregpie@gmail.com>
2020-06-15 11:35:17 +0200

    permutation via shuffled combination

--- src/JustMyLuck.pickPermutation.js
+++ src/JustMyLuck.pickPermutation.js
@@ -1,33 +1,10 @@
-import './JustMyLuck.pick';
-import './JustMyLuck.pickIndex';
+import './JustMyLuck.pickCombination';
 import './JustMyLuck.shuffleInPlace';
 import JustMyLuck from './JustMyLuck';
 
-import Array_fromExceptLike from './core/Array/fromExceptLike';
-
 JustMyLuck.extend({
 	pickPermutation(collection, k) {
-		let array = Array_fromExceptLike(collection);
-		let n = array.length;
-		if (n > 1 && k > 1) {
-			if (array === collection) {
-				array = Array.from(array);
-			}
-			if (k < n) {
-				let result = [];
-				for (; k > 0; k--) {
-					let index = this.pickIndex(array);
-					let value = array.splice(index, 1)[0];
-					result.push(value);
-				}
-				return result;
-			}
-			return this.shuffleInPlace(array);
-		}
-		if (n > 0 && k > 0) {
-			return [this.pick(array)];
-		}
-		return [];
+		return this.shuffleInPlace(this.pickCombination(collection, k));
 	},
 	permutation(...args) {
 		return this.pickPermutation(...args);
